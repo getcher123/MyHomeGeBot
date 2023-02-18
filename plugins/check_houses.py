b8e0d7e5e32d58a9dfcab757f60bf27ef7d6882f
getcher123 <getcher123@gmail.com>
2023-02-18 17:45:55 +0300

    users id fix

--- plugins/check_houses.py
+++ plugins/check_houses.py
@@ -12,7 +12,8 @@ from settings.debug_settings import LOGGING_LEVEL
 
 logging.basicConfig(level=LOGGING_LEVEL)
 
-first_time : bool
+first_time: bool
+
 
 async def check_new_houses(dp: Dispatcher, sleep_time: int):
     while True:
@@ -27,15 +28,16 @@ async def check_new_houses(dp: Dispatcher, sleep_time: int):
         if p.status == 200:
             logging.debug(f'status code: {p.status}')
         else:
-            logging.warning(f'Oh shit... We have a problem, status code: {p.status}')
+            logging.warning(
+                f'Oh shit... We have a problem, status code: {p.status}')
             continue
 
         p.get_cards()
         p.get_homes_url_and_images()
-        
+
         if not len(p.homes_url):
             continue
-        
+
         p.save_to_env()
 
         if p.first_time:
@@ -48,6 +50,9 @@ async def check_new_houses(dp: Dispatcher, sleep_time: int):
             # Download the image and sends it
             response = requests.get(image_url)
             user_ids = os.environ.get('USER_IDS', '').split(',')
+            if not user_ids:
+                logging.exception('Users ID is not founded')
+                continue
 
             for user_id in user_ids:
                 try:
@@ -57,5 +62,3 @@ async def check_new_houses(dp: Dispatcher, sleep_time: int):
                     await dp.bot.send_photo(user_id, photo=image_bytes_copy, caption=msg, parse_mode="Markdown")
                 except Exception as e:
                     logging.exception('Sending msg error')
-        
-
