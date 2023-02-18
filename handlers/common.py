3d0fead28ae9821dc8ab7bc91d2fd4ad239d93a0
getcher123 <getcher123@gmail.com>
2023-02-18 18:44:28 +0300

    new feature

--- handlers/common.py
+++ handlers/common.py
@@ -1,14 +1,19 @@
 """Сommon handlers and registration"""
 import logging
 import os
+from io import BytesIO
+
 
 from aiogram import types
 from aiogram.dispatcher import Dispatcher
 from aiogram.dispatcher import FSMContext
+import requests
+
 
 from keyboards import set_link_keyboard
 from messages import MESSAGES
 from states import Form
+from home_parser import MyHomeParser
 
 
 class CommonHandlers:
@@ -26,7 +31,7 @@ class CommonHandlers:
             MESSAGES['start'].format(message.from_user.username),
             reply_markup=set_link_keyboard
         )
-
+        
         # Add the user ID to the environment variable
         if (not os.environ.get('USER_IDS')):
             logging.warning("# os.environ.get('USER_IDS')!")
@@ -38,6 +43,53 @@ class CommonHandlers:
             user_ids.append(str(message.chat.id))
             os.environ['USER_IDS'] = ','.join(user_ids)
             await message.answer("Let's get started!🔥")
+        
+    async def show_all(message: types.Message) -> None:
+        """
+        Handler of the /start command
+
+        Args:
+            message (types.Message): Instance of the Message class.
+        """
+        url = os.environ.get('URL')
+        if not url:
+            logging.warning("# not url!")
+            return
+
+        p = MyHomeParser(url)
+
+        if p.status == 200:
+            logging.debug(f'status code: {p.status}')
+        else:
+            logging.warning(
+                f'Oh shit... We have a problem, status code: {p.status}')
+            return
+
+        p.get_cards()
+        p.get_homes_url_and_images()
+
+        if not len(p.homes_url):
+            return
+
+        for i, url in enumerate(p.homes_url):
+            msg = f"**[{p.description['title'][i]}]({url})** - \n*${p.description['price'][i]}*     {p.description['square'][i]}     {p.description['stairs'][i]} \n{p.description['address'][i]}"
+            image_url = p.description['image_url'][i]
+
+            # Download the image and sends it
+            response = requests.get(image_url)
+            user_ids = os.environ.get('USER_IDS', '').split(',')
+            if not user_ids:
+                logging.error('Users ID is not founded')
+                continue
+
+            for user_id in user_ids:
+                try:
+                    logging.info(f'# send_photo {user_id = }')
+                    image_bytes_copy = BytesIO(response.content)
+                    image_bytes_copy.seek(0)
+                    await dp.bot.send_photo(user_id, photo=image_bytes_copy, caption=msg, parse_mode="Markdown")
+                except Exception as e:
+                    logging.exception('Sending msg error')    
 
     async def help_command(message: types.Message) -> None: 
         """
@@ -84,6 +136,8 @@ def register_client_handlers(dp: Dispatcher) -> None:
     dp.register_message_handler(CommonHandlers.start_command, commands=['start'])
     dp.register_message_handler(CommonHandlers.help_command, commands=['help'])
     dp.register_message_handler(CommonHandlers.set_link, commands=['set_link'])
+    dp.register_message_handler(CommonHandlers.show_all, commands=['show_all'])
+
     dp.register_message_handler(CommonHandlers.set_link,  lambda message: message.text in ['Задать ссылку для поиска', 'Обновить ссылку для поиска'])
     
     dp.register_message_handler(CommonHandlers.cancel_command, commands=['cancel'], state='*')
